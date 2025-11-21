9fb931df0a80e8c75b335c4c3462639c153f49be
AndouKanade <k024g0073@g.neec.ac.jp>
2025-11-21 11:48:01 +0900

    きちゃ

--- Project/main.cpp
+++ Project/main.cpp
@@ -1405,7 +1405,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
 
   blendDesc.RenderTarget[0].RenderTargetWriteMask =
       D3D12_COLOR_WRITE_ENABLE_ALL;
-  blendDesc.RenderTarget[0].BlendEnable = true;
+  blendDesc.RenderTarget[0].BlendEnable = TRUE;
   blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
   blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
   blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
