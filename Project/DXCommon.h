e9cf30f91a288427683ce6feaded0625d403d2b8
AndouKanade <k024g0073@g.neec.ac.jp>
2025-12-01 14:28:51 +0900

    05_02までok

--- Project/DXCommon.h
+++ Project/DXCommon.h
@@ -13,120 +13,120 @@
 class DXCommon {
 public:
 #pragma region メンバ変数
-  Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
-  Microsoft::WRL::ComPtr<ID3D12Device> device;
+	Microsoft::WRL::ComPtr<IDXGIFactory7> dxgiFactory;
+	Microsoft::WRL::ComPtr<ID3D12Device> device;
 
-  Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator = nullptr;
-  Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList = nullptr;
-  Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12CommandQueue> commandQueue = nullptr;
 
-  Microsoft::WRL::ComPtr<IDXGISwapChain4> swapChain = nullptr;
-  DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
-  std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, 2> swapChainResources;
+	Microsoft::WRL::ComPtr<IDXGISwapChain4> swapChain = nullptr;
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
+	std::array<Microsoft::WRL::ComPtr<ID3D12Resource>,2> swapChainResources;
 
-  Microsoft::WRL::ComPtr<ID3D12Resource> depthStencilResource = nullptr;
-  D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle;
+	Microsoft::WRL::ComPtr<ID3D12Resource> depthStencilResource = nullptr;
+	D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle;
 
-  uint32_t descriptorSizeSRV = 0;
-  uint32_t descriptorSizeRTV = 0;
-  uint32_t descriptorSizeDSV = 0;
-  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap = nullptr;
-  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvDescriptorHeap = nullptr;
-  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> dsvDescriptorHeap = nullptr;
+	uint32_t descriptorSizeSRV = 0;
+	uint32_t descriptorSizeRTV = 0;
+	uint32_t descriptorSizeDSV = 0;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> srvDescriptorHeap = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> dsvDescriptorHeap = nullptr;
 
-  D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
-  D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
-  std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, 2> backBuffers;
+	D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+	std::array<Microsoft::WRL::ComPtr<ID3D12Resource>,2> backBuffers;
 
-  Microsoft::WRL::ComPtr<ID3D12Fence> fence = nullptr;
+	Microsoft::WRL::ComPtr<ID3D12Fence> fence = nullptr;
 
-  D3D12_VIEWPORT viewport{};
-  D3D12_RECT scissorRect{};
+	D3D12_VIEWPORT viewport{};
+	D3D12_RECT scissorRect{};
 
-  Microsoft::WRL::ComPtr<IDxcUtils> dxcUtils = nullptr;
-  Microsoft::WRL::ComPtr<IDxcCompiler3> dxcCompiler = nullptr;
-  IDxcIncludeHandler *includeHandler = nullptr;
+	Microsoft::WRL::ComPtr<IDxcUtils> dxcUtils = nullptr;
+	Microsoft::WRL::ComPtr<IDxcCompiler3> dxcCompiler = nullptr;
+	IDxcIncludeHandler* includeHandler = nullptr;
 
-  D3D12_RESOURCE_BARRIER barrier{};
-  uint64_t fenceValue = 0;
-  HANDLE fenceEvent = nullptr;
+	D3D12_RESOURCE_BARRIER barrier{};
+	uint64_t fenceValue = 0;
+	HANDLE fenceEvent = nullptr;
 
 #pragma endregion
 
 #pragma region メンバ関数
 
-  void Initialize(WinAPI *winApi);
+	void Initialize(WinAPI* winApi);
 
-  void InitDevice();
-  void InitCommand();
-  void CreateSwapChain();
-  void CreateDepthBuffer();
-  void CreateDescriptorHeaps();
-  void InitRenderTargetView();
-  void InitDepthStancilView();
-  void InitFence();
-  void InitViewportRect();
-  void InitScissorRect();
-  void CreateDXCCompiler();
-  void InitImGui();
+	void InitDevice();
+	void InitCommand();
+	void CreateSwapChain();
+	void CreateDepthBuffer();
+	void CreateDescriptorHeaps();
+	void InitRenderTargetView();
+	void InitDepthStancilView();
+	void InitFence();
+	void InitViewportRect();
+	void InitScissorRect();
+	void CreateDXCCompiler();
+	void InitImGui();
 
-  void PreDraw();
-  void PostDraw();
+	void PreDraw();
+	void PostDraw();
 
 #pragma endregion
 
 #pragma region ユーティリティ関数
-  Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>
-  CreateDiscriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE heapType, UINT numDescriptors,
-                       bool shaderVisible);
+	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>
+		CreateDiscriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE heapType,UINT numDescriptors,
+			bool shaderVisible);
 
-  D3D12_CPU_DESCRIPTOR_HANDLE GetSRVCPUDescriptorHandle(uint32_t index);
-  D3D12_GPU_DESCRIPTOR_HANDLE GetSRVGPUDescriptorHandle(uint32_t index);
+	D3D12_CPU_DESCRIPTOR_HANDLE GetSRVCPUDescriptorHandle(uint32_t index);
+	D3D12_GPU_DESCRIPTOR_HANDLE GetSRVGPUDescriptorHandle(uint32_t index);
 
-  Microsoft::WRL::ComPtr<IDxcBlob> CompileShader(const std::wstring &filePath,
-                                                 const wchar_t *profile);
+	Microsoft::WRL::ComPtr<IDxcBlob> CompileShader(const std::wstring& filePath,
+		const wchar_t* profile);
 
-  Microsoft::WRL::ComPtr<ID3D12Resource>
-  CreateBufferResource(size_t sizeInBytes);
+	Microsoft::WRL::ComPtr<ID3D12Resource>
+		CreateBufferResource(size_t sizeInBytes);
 
-  Microsoft::WRL::ComPtr<ID3D12Resource>
-  CreateTextureResource(const DirectX::TexMetadata &metadata);
+	Microsoft::WRL::ComPtr<ID3D12Resource>
+		CreateTextureResource(const DirectX::TexMetadata& metadata);
 
-  Microsoft::WRL::ComPtr<ID3D12Resource>
-  UploadTextureData(const Microsoft::WRL::ComPtr<ID3D12Resource> &texture,
-                    const DirectX::ScratchImage &mipImages);
+	Microsoft::WRL::ComPtr<ID3D12Resource>
+		UploadTextureData(const Microsoft::WRL::ComPtr<ID3D12Resource>& texture,
+			const DirectX::ScratchImage& mipImages);
 
-  static DirectX::ScratchImage LoadTexture(const std::string &filePath);
+	static DirectX::ScratchImage LoadTexture(const std::string& filePath);
 #pragma endregion
 
-  ID3D12Device *GetDevice() const { return device.Get(); }
-  ID3D12GraphicsCommandList *GetCommandList() const {
-    return commandList.Get();
-  }
+	ID3D12Device* GetDevice() const { return device.Get(); }
+	ID3D12GraphicsCommandList* GetCommandList() const {
+		return commandList.Get();
+	}
 
 private:
-  WinAPI *winApi_ = nullptr;
+	WinAPI* winApi_ = nullptr;
 
-  static D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(
-      const Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> &descriptorHeap,
-      uint32_t descriptorSize, uint32_t index) {
-    D3D12_CPU_DESCRIPTOR_HANDLE handleCPU =
-        descriptorHeap->GetCPUDescriptorHandleForHeapStart();
-    handleCPU.ptr += descriptorSize * index;
-    return handleCPU;
-  }
+	static D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(
+		const Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>& descriptorHeap,
+		uint32_t descriptorSize,uint32_t index) {
+		D3D12_CPU_DESCRIPTOR_HANDLE handleCPU =
+			descriptorHeap->GetCPUDescriptorHandleForHeapStart();
+		handleCPU.ptr += descriptorSize * index;
+		return handleCPU;
+	}
 
-  static D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDscriptorHandle(
-      const Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> &descriptorHeap,
-      uint32_t descriptorSize, uint32_t index) {
-    D3D12_GPU_DESCRIPTOR_HANDLE handleGPU =
-        descriptorHeap->GetGPUDescriptorHandleForHeapStart();
-    handleGPU.ptr += descriptorSize * index;
-    return handleGPU;
-  }
+	static D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDscriptorHandle(
+		const Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>& descriptorHeap,
+		uint32_t descriptorSize,uint32_t index) {
+		D3D12_GPU_DESCRIPTOR_HANDLE handleGPU =
+			descriptorHeap->GetGPUDescriptorHandleForHeapStart();
+		handleGPU.ptr += descriptorSize * index;
+		return handleGPU;
+	}
 
-  void InitializeFixFPS();
-  void UpdateFixFPS();
+	void InitializeFixFPS();
+	void UpdateFixFPS();
 
-  std::chrono::steady_clock::time_point reference_;
+	std::chrono::steady_clock::time_point reference_;
 };
---
+++ Project/SpriteCommon.cpp
@@ -0,0 +1,186 @@
+#include "SpriteCommon.h"
+#include"Logger.h"
+#include <d3dx12.h>
+
+void SpriteCommon::Initialize(DXCommon* dxCommon)
+{
+	SpriteCommon::dxCommon_ = dxCommon;
+	CreatePipelineState();
+}
+
+void SpriteCommon::Draw()
+{
+
+	dxCommon_->commandList.Get()->SetGraphicsRootSignature(
+		rootSignature.Get());
+	dxCommon_->commandList.Get()->SetPipelineState(
+		graphicsPipelineState.Get());
+	dxCommon_->commandList.Get()->IASetPrimitiveTopology(
+		D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+}
+
+void SpriteCommon::CreateRootSignature()
+{
+
+	HRESULT hr;
+	descriptionRootSignature.Flags =
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+	descriptorRange[0].BaseShaderRegister = 0;
+	descriptorRange[0].NumDescriptors = 1;
+	descriptorRange[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+	descriptorRange[0].OffsetInDescriptorsFromTableStart =
+		D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+	rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	rootParameters[0].Descriptor.ShaderRegister = 0;
+
+	rootParameters[1].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[1].ShaderVisibility = D3D12_SHADER_VISIBILITY_VERTEX;
+	rootParameters[1].Descriptor.ShaderRegister = 1;
+
+	rootParameters[2].ParameterType = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+	rootParameters[2].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	rootParameters[2].DescriptorTable.pDescriptorRanges = descriptorRange;
+	rootParameters[2].DescriptorTable.NumDescriptorRanges =
+		_countof(descriptorRange);
+
+	rootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;
+	rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	rootParameters[3].Descriptor.ShaderRegister = 2;
+
+	descriptionRootSignature.pParameters = rootParameters;
+	descriptionRootSignature.NumParameters = _countof(rootParameters);
+
+
+	depthStencilDesc.DepthEnable = true;
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
+	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+
+
+
+	staticSamplers[0].Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
+	staticSamplers[0].AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+	staticSamplers[0].ComparisonFunc = D3D12_COMPARISON_FUNC_NEVER;
+	staticSamplers[0].MinLOD = 0.0f;
+	staticSamplers[0].MaxLOD = D3D12_FLOAT32_MAX;
+	staticSamplers[0].ShaderRegister = 0;
+	staticSamplers[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+	descriptionRootSignature.pStaticSamplers = staticSamplers;
+	descriptionRootSignature.NumStaticSamplers = _countof(staticSamplers);
+
+
+	hr = D3D12SerializeRootSignature(&descriptionRootSignature,
+		D3D_ROOT_SIGNATURE_VERSION_1,&signatureBlob,
+		&errorBlob);
+
+	if(FAILED(hr)) {
+		Logger::Log(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+		assert(false);
+	}
+
+	hr = dxCommon_->GetDevice()->CreateRootSignature(
+		0,signatureBlob->GetBufferPointer(),signatureBlob->GetBufferSize(),
+		IID_PPV_ARGS(&rootSignature));
+	assert(SUCCEEDED(hr));
+
+
+
+#pragma endregion
+
+}
+
+void SpriteCommon::CreatePipelineState()
+{
+
+	HRESULT hr;
+
+	void CreatePipelineState();
+
+#pragma region InputLayoutを生成する
+	inputElementDescs[0].SemanticName = "POSITION";
+	inputElementDescs[0].SemanticIndex = 0;
+	inputElementDescs[0].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	inputElementDescs[0].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	inputElementDescs[1].SemanticName = "TEXCOORD";
+	inputElementDescs[1].SemanticIndex = 0;
+	inputElementDescs[1].Format = DXGI_FORMAT_R32G32_FLOAT;
+	inputElementDescs[1].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+	inputElementDescs[2].SemanticName = "NORMAL";
+	inputElementDescs[2].SemanticIndex = 0;
+	inputElementDescs[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	inputElementDescs[2].AlignedByteOffset = D3D12_APPEND_ALIGNED_ELEMENT;
+
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+#pragma endregion
+
+#pragma region BlendStateを生成する
+
+
+	blendDesc.RenderTarget[0].RenderTargetWriteMask =
+		D3D12_COLOR_WRITE_ENABLE_ALL;
+	blendDesc.RenderTarget[0].BlendEnable = TRUE;
+	blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+	blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+	blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+	blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+	blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+
+#pragma endregion
+
+#pragma region RasterrizerStateを生成する
+
+	resterizerDesc.CullMode = D3D12_CULL_MODE_BACK;
+	resterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+#pragma endregion
+
+#pragma region shaderをコンパイルする
+	vertexShaderBlob =
+		dxCommon_->CompileShader(L"resource/shader/Object3d.VS.hlsl",L"vs_6_0");
+	assert(vertexShaderBlob != nullptr);
+	pixelShaderBlob =
+		dxCommon_->CompileShader(L"resource/shader/Object3d.PS.hlsl",L"ps_6_0");
+	assert(pixelShaderBlob != nullptr);
+
+#pragma endregion
+
+#pragma region PSOを生成する
+
+	graphicsPipelineStateDesc.pRootSignature = rootSignature.Get();
+	graphicsPipelineStateDesc.InputLayout = inputLayoutDesc;
+	graphicsPipelineStateDesc.VS = {vertexShaderBlob->GetBufferPointer(),
+									vertexShaderBlob->GetBufferSize()};
+	graphicsPipelineStateDesc.PS = {pixelShaderBlob->GetBufferPointer(),
+									pixelShaderBlob->GetBufferSize()};
+	graphicsPipelineStateDesc.BlendState = blendDesc;
+	graphicsPipelineStateDesc.RasterizerState = resterizerDesc;
+
+	// 書き込むRTVの情報
+	graphicsPipelineStateDesc.NumRenderTargets = 1;
+	graphicsPipelineStateDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+
+	graphicsPipelineStateDesc.DepthStencilState = depthStencilDesc;
+	graphicsPipelineStateDesc.DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+
+	// 利用する形状のタイプ(今回は三角形)
+	graphicsPipelineStateDesc.PrimitiveTopologyType =
+		D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	//  どのような画面に色を打ち込むかの設定
+	graphicsPipelineStateDesc.SampleDesc.Count = 1;
+	graphicsPipelineStateDesc.SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+	//  実際に生成
+	hr = dxCommon_->GetDevice()->CreateGraphicsPipelineState(
+		&graphicsPipelineStateDesc,IID_PPV_ARGS(&graphicsPipelineState));
+	assert(SUCCEEDED(hr));
+
+#pragma endregion
+
+
+}
