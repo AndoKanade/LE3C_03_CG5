a8b0e33a1a12be1ee309b7567499a4464aff42bf
Kanade Andou <k024g0073@g.neec.ac.jp>
2026-05-13 11:29:56 +0900

    整理してできたと思う

--- Project/Engine/Base/DXCommon.h
+++ Project/Engine/Base/DXCommon.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "WinAPI.h"
 #include <d3d12.h>
 #include <dxgi1_6.h>
@@ -23,58 +22,57 @@ class DXCommon{
 public:
     template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 
-#pragma region メンバ変数
-public:
-    // --- Direct3D Core ---
+public: // --- メンバリソース ---
+
+    // Direct3D 12 Core
     ComPtr<IDXGIFactory7> dxgiFactory;
     ComPtr<ID3D12Device> device;
 
-    // --- Command Objects ---
+    // Command Objects
     ComPtr<ID3D12CommandAllocator> commandAllocator = nullptr;
     ComPtr<ID3D12GraphicsCommandList> commandList = nullptr;
     ComPtr<ID3D12CommandQueue> commandQueue = nullptr;
 
-    // --- SwapChain & RenderTargets ---
+    // SwapChain & BackBuffers
     ComPtr<IDXGISwapChain4> swapChain = nullptr;
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc{};
     std::array<ComPtr<ID3D12Resource>,2> swapChainResources;
 
-    // --- RTV Descriptor Heap ---
+    // RTV (Render Target View)
     ComPtr<ID3D12DescriptorHeap> rtvDescriptorHeap = nullptr;
     D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
     D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
     uint32_t descriptorSizeRTV = 0;
 
-    // --- Depth Stencil ---
+    // DSV (Depth Stencil View)
     ComPtr<ID3D12Resource> depthStencilResource = nullptr;
     ComPtr<ID3D12DescriptorHeap> dsvDescriptorHeap = nullptr;
     D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle;
     uint32_t descriptorSizeDSV = 0;
 
-    // --- 深度SRV用ハンドル (ポストプロセスで使用) ---
+    // SRV (Shader Resource View) for PostProcess
     D3D12_GPU_DESCRIPTOR_HANDLE depthSrvHandleGpu_;
 
-    // --- Synchronization (Fence) ---
+    // Synchronization
     ComPtr<ID3D12Fence> fence = nullptr;
     uint64_t fenceValue = 0;
     HANDLE fenceEvent = nullptr;
     D3D12_RESOURCE_BARRIER barrier{};
 
-    // --- Viewport & Scissor ---
+    // Rendering States
     D3D12_VIEWPORT viewport{};
     D3D12_RECT scissorRect{};
 
-    // --- DXC (Shader Compiler) ---
+    // DXC (Shader Compiler)
     ComPtr<IDxcUtils> dxcUtils = nullptr;
     ComPtr<IDxcCompiler3> dxcCompiler = nullptr;
     IDxcIncludeHandler* includeHandler = nullptr;
-#pragma endregion
 
-#pragma region メンバ関数
-public:
-    // --- 初期化フロー ---
+public: // --- 初期化・描画フロー ---
+
     void Initialize(WinAPI* winApi);
 
+    // 初期化ステップ
     void InitDevice();
     void InitCommand();
     void CreateSwapChain();
@@ -88,40 +86,38 @@ public:
     void InitScissorRect();
     void CreateDXCCompiler();
 
-    // --- 描画フロー (RenderTexture対応) ---
-    void PreDraw(RenderTexture* renderTexture = nullptr); 
+    // フレーム描画
+    void PreDraw(RenderTexture* renderTexture = nullptr);
     void PostDraw();
 
-    // --- ユーティリティ関数 ---
+public: // --- ユーティリティ・リソース生成 ---
+
     ComPtr<ID3D12DescriptorHeap> CreateDiscriptorHeap(D3D12_DESCRIPTOR_HEAP_TYPE heapType,UINT numDescriptors,bool shaderVisible);
     ComPtr<IDxcBlob> CompileShader(const std::wstring& filePath,const wchar_t* profile);
     ComPtr<ID3D12Resource> CreateBufferResource(size_t sizeInBytes);
     ComPtr<ID3D12Resource> CreateTextureResource(const DirectX::TexMetadata& metadata);
     ComPtr<ID3D12Resource> UploadTextureData(const ComPtr<ID3D12Resource>& texture,const DirectX::ScratchImage& mipImages);
 
-    // --- アクセッサ ---
+public: // --- アクセッサ ---
+
     ID3D12Device* GetDevice() const{ return device.Get(); }
     ID3D12GraphicsCommandList* GetCommandList() const{ return commandList.Get(); }
     ID3D12CommandQueue* GetCommandQueue() const{ return commandQueue.Get(); }
     size_t GetSwapChainResourcesNum() const{ return swapChainResources.size(); }
-    D3D12_CPU_DESCRIPTOR_HANDLE GetDsvHandle() const{ return dsvHandle; } 
-    // 深度SRVのハンドルを返すアクセサを追加
-    D3D12_GPU_DESCRIPTOR_HANDLE GetDepthSrvHandleGpu() const{ return depthSrvHandleGpu_; } 
+    D3D12_CPU_DESCRIPTOR_HANDLE GetDsvHandle() const{ return dsvHandle; }
+    D3D12_GPU_DESCRIPTOR_HANDLE GetDepthSrvHandleGpu() const{ return depthSrvHandleGpu_; }
 
-    // ハンドルをセットする関数（初期化時に使用）
-    void SetDepthSrvHandleGpu(D3D12_GPU_DESCRIPTOR_HANDLE handle){ depthSrvHandleGpu_ = handle; } 
+    void SetDepthSrvHandleGpu(D3D12_GPU_DESCRIPTOR_HANDLE handle){ depthSrvHandleGpu_ = handle; }
 
-#pragma endregion
+private: // --- 内部処理・FPS制御 ---
 
-private:
     WinAPI* winApi_ = nullptr;
-
-    // --- FPS制御 ---
     std::chrono::steady_clock::time_point reference_;
+
     void InitializeFixFPS();
     void UpdateFixFPS();
 
-    // --- ヘルパー (static) ---
+    // 静的ヘルパー関数
     static D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandle(const ComPtr<ID3D12DescriptorHeap>& descriptorHeap,uint32_t descriptorSize,uint32_t index){
         D3D12_CPU_DESCRIPTOR_HANDLE handleCPU = descriptorHeap->GetCPUDescriptorHandleForHeapStart();
         handleCPU.ptr += (size_t)descriptorSize * index;
--- Project/Engine/Framework/Application.cpp
+++ Project/Engine/Framework/Application.cpp
@@ -2,158 +2,155 @@
 #include "SceneFactory.h"
 #include "SrvManager.h"
 
-// 静的メンバ変数の実体定義
+// 静的メンバ変数の実体
 Application* Application::instance_ = nullptr;
 
 Application::Application(){
-	instance_ = this; // シングルトンインスタンスの登録
+    instance_ = this;
 }
 
 Application::~Application() = default;
 
 // --- 初期化処理 ---
 void Application::Initialize(){
-	// 1. 基底クラスの初期化
-	Framework::Initialize();
-	dxCommon_->InitDepthShaderResourceView(); 
+    // 1. 基底クラスの初期化
+    Framework::Initialize();
+    dxCommon_->InitDepthShaderResourceView();
 
-	// 2. ポストプロセス用リソースの初期化
-	postProcess_ = std::make_unique<PostProcess>();
-	postProcess_->Initialize(dxCommon_.get());
+    // 2. ポストプロセス関連のリソース初期化
+    postProcess_ = std::make_unique<PostProcess>();
+    postProcess_->Initialize(dxCommon_.get());
 
-	renderTexture_ = std::make_unique<RenderTexture>();
+    renderTexture_ = std::make_unique<RenderTexture>();
 
-	// RTVハンドルの取得 (バックバッファ2つの次、インデックス2を使用)
-	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = dxCommon_->rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
-	rtvHandle.ptr += (size_t)dxCommon_->descriptorSizeRTV * 2;
+    // RTVハンドルの取得
+    D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = dxCommon_->rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
+    rtvHandle.ptr += (size_t)dxCommon_->descriptorSizeRTV * 2;
 
-	// SRVハンドルの取得 (SrvManagerから空きを確保)
-	uint32_t srvIndex = SrvManager::GetInstance()->Allocate();
-	D3D12_CPU_DESCRIPTOR_HANDLE srvHandleCpu = SrvManager::GetInstance()->GetCPUDescriptorHandle(srvIndex);
-	D3D12_GPU_DESCRIPTOR_HANDLE srvHandleGpu = SrvManager::GetInstance()->GetGPUDescriptorHandle(srvIndex);
+    // SRVハンドルの取得
+    uint32_t srvIndex = SrvManager::GetInstance()->Allocate();
+    D3D12_CPU_DESCRIPTOR_HANDLE srvHandleCpu = SrvManager::GetInstance()->GetCPUDescriptorHandle(srvIndex);
+    D3D12_GPU_DESCRIPTOR_HANDLE srvHandleGpu = SrvManager::GetInstance()->GetGPUDescriptorHandle(srvIndex);
 
-	// レンダーテクスチャ生成
-	renderTexture_->Create(
-		dxCommon_->GetDevice(),
-		WinAPI::kClientWidth,
-		WinAPI::kCliantHeight,
-		DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
-		{0.1f, 0.25f, 0.5f, 1.0f},
-		rtvHandle,
-		srvHandleCpu,
-		srvHandleGpu
-	);
+    // オフスクリーン用テクスチャの生成
+    renderTexture_->Create(
+        dxCommon_->GetDevice(),
+        WinAPI::kClientWidth,
+        WinAPI::kCliantHeight,
+        DXGI_FORMAT_R8G8B8A8_UNORM_SRGB,
+        {0.1f, 0.25f, 0.5f, 1.0f},
+        rtvHandle,
+        srvHandleCpu,
+        srvHandleGpu
+    );
 
-	// 3. シーン系の初期化
-	sceneFactory_ = std::make_unique<SceneFactory>();
-	SceneManager* sceneManager = SceneManager::GetInstance();
-	sceneManager->SetFactory(sceneFactory_.get());
-	sceneManager->SetCommonPtr(object3dCommon_.get(),input_.get(),spriteCommon_.get());
-	sceneManager->ChangeScene("TITLE");
+    // 3. シーン管理システムの初期化
+    sceneFactory_ = std::make_unique<SceneFactory>();
+    SceneManager* sceneManager = SceneManager::GetInstance();
+    sceneManager->SetFactory(sceneFactory_.get());
+    sceneManager->SetCommonPtr(object3dCommon_.get(),input_.get(),spriteCommon_.get());
+    sceneManager->ChangeScene("TITLE");
 }
 
 void Application::Finalize(){
-	Framework::Finalize();
+    Framework::Finalize();
 }
 
 void Application::Update(){
-	Framework::Update();
+    Framework::Update();
 }
 
+// --- 描画処理 ---
 void Application::Draw(){
-	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+    ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
-	// --- パス1：RenderTextureへの描画 (オフスクリーン) ---
-	dxCommon_->PreDraw(renderTexture_.get());
-	SrvManager::GetInstance()->PreDraw();
+    // 1. RenderTextureへの描画 (パス1)
+    dxCommon_->PreDraw(renderTexture_.get());
+    SrvManager::GetInstance()->PreDraw();
 
-	if(object3dCommon_){
-		object3dCommon_->Draw();
-	}
-	SceneManager::GetInstance()->Draw();
+    if(object3dCommon_){
+        object3dCommon_->Draw();
+    }
+    SceneManager::GetInstance()->Draw();
 
-	// --- パス2：Swapchainへの描画 (ポストプロセス適用) ---
-	dxCommon_->PreDraw(nullptr); // 描画先をバックバッファに切り替え
+    // 2. Swapchainへの描画 (パス2：ポストプロセス適用)
+    dxCommon_->PreDraw(nullptr);
 
-	// 1. カラーと深度の状態を「テクスチャ用(SRV)」へ一括遷移
-	D3D12_RESOURCE_BARRIER barriers[2] = {};
+    // リソースバリアの設定：カラーと深度を読み取り状態へ遷移
+    D3D12_RESOURCE_BARRIER barriers[2] = {};
 
-	// [0] RenderTexture (カラーバッファ)
-	barriers[0].Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-	barriers[0].Transition.pResource = renderTexture_->GetResource();
-	barriers[0].Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
-	barriers[0].Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+    // [0] カラーバッファの遷移
+    barriers[0].Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+    barriers[0].Transition.pResource = renderTexture_->GetResource();
+    barriers[0].Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+    barriers[0].Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
 
-	// [1] DepthBuffer (深度バッファ) ★追加
-	barriers[1].Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-	barriers[1].Transition.pResource = dxCommon_->depthStencilResource.Get(); // 深度リソース
-	barriers[1].Transition.StateBefore = D3D12_RESOURCE_STATE_DEPTH_WRITE;
-	barriers[1].Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+    // [1] 深度バッファの遷移
+    barriers[1].Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+    barriers[1].Transition.pResource = dxCommon_->depthStencilResource.Get();
+    barriers[1].Transition.StateBefore = D3D12_RESOURCE_STATE_DEPTH_WRITE;
+    barriers[1].Transition.StateAfter = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
 
-	commandList->ResourceBarrier(2,barriers);
+    commandList->ResourceBarrier(2,barriers);
 
-	// 2. ポストプロセス実行
-	postProcess_->Draw(commandList,renderTexture_->GetSrvHandleGpu(),dxCommon_->GetDepthSrvHandleGpu(),currentPPType_);
+    // ポストプロセスの実行
+    postProcess_->Draw(commandList,renderTexture_->GetSrvHandleGpu(),dxCommon_->GetDepthSrvHandleGpu(),currentPPType_);
 
-	// 3. 次のフレームのために状態を元に戻す
-	barriers[0].Transition.StateBefore = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
-	barriers[0].Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+    // 次フレームのために状態を元に戻す
+    barriers[0].Transition.StateBefore = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+    barriers[0].Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
 
-	barriers[1].Transition.StateBefore = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
-	barriers[1].Transition.StateAfter = D3D12_RESOURCE_STATE_DEPTH_WRITE;
+    barriers[1].Transition.StateBefore = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+    barriers[1].Transition.StateAfter = D3D12_RESOURCE_STATE_DEPTH_WRITE;
 
-	commandList->ResourceBarrier(2,barriers);
+    commandList->ResourceBarrier(2,barriers);
 
 #ifdef _DEBUG
-	ImGuiManager::GetInstance()->Draw();
+    ImGuiManager::GetInstance()->Draw();
 #endif
 
-	dxCommon_->PostDraw();
+    dxCommon_->PostDraw();
 }
 
 // --- デバッグUI表示 ---
 void Application::ShowPostProcessUI(){
 #ifdef _DEBUG
-	ImGui::Begin("PostProcess Settings");
+    ImGui::Begin("PostProcess Settings");
 
-	// フィルター切り替え
-	const char* typeNames[] = {"Default(PostProcess)", "BoxFilter", "Grayscale", "Vignette", "GaussianBlur", "Outline"};
-	int currentIdx = static_cast<int>(currentPPType_);
+    // フィルターの切り替え
+    const char* typeNames[] = {"Default", "BoxFilter", "Grayscale", "Vignette", "GaussianBlur", "LuminanceOutline", "DepthOutline"};
+    int currentIdx = static_cast<int>(currentPPType_);
 
-	if(ImGui::Combo("Filter Type",&currentIdx,typeNames,IM_ARRAYSIZE(typeNames))){
-		currentPPType_ = static_cast<PostProcess::Type>(currentIdx);
-	}
+    if(ImGui::Combo("Filter Type",&currentIdx,typeNames,IM_ARRAYSIZE(typeNames))){
+        currentPPType_ = static_cast<PostProcess::Type>(currentIdx);
+    }
 
-	ImGui::Separator();
+    ImGui::Separator();
 
-	// パラメータ調整
-	if(currentPPType_ == PostProcess::Type::Vignette){
-		ImGui::Text("Vignette Settings");
+    // フィルタごとのパラメータ調整
+    if(currentPPType_ == PostProcess::Type::Vignette){
+        ImGui::Text("Vignette Settings");
+        static float intensity = 0.5f;
+        if(ImGui::DragFloat("Intensity",&intensity,0.01f,0.0f,1.0f)){
+            postProcess_->SetVignetteIntensity(intensity);
+        }
+        static float scale = 0.8f;
+        if(ImGui::DragFloat("Scale",&scale,0.01f,0.0f,2.0f)){
+            postProcess_->SetVignetteScale(scale);
+        }
+    } else if(currentPPType_ == PostProcess::Type::BoxFilter ||
+        currentPPType_ == PostProcess::Type::GaussianBlur){
+        static int k = 1;
+        const char* label = (currentPPType_ == PostProcess::Type::BoxFilter)?"Kernel Size":"Blur Strength";
+        if(ImGui::SliderInt(label,&k,0,10)){
+            postProcess_->SetKernelSize(k);
+        }
+    } else if(currentPPType_ == PostProcess::Type::LuminanceOutline ||
+        currentPPType_ == PostProcess::Type::DepthOutline){
+        ImGui::Text(currentPPType_ == PostProcess::Type::DepthOutline?"Depth Edge Settings":"Luminance Edge Settings");
+        ImGui::Text("Edge detection active.");
+    }
 
-		static float intensity = 0.5f;
-		if(ImGui::DragFloat("Intensity",&intensity,0.01f,0.0f,1.0f)){
-			postProcess_->SetVignetteIntensity(intensity);
-		}
-
-		static float scale = 0.8f;
-		// 修正：SetKernelSize になっていたのを SetVignetteScale に変更
-		if(ImGui::DragFloat("Scale",&scale,0.01f,0.0f,2.0f)){
-			postProcess_->SetVignetteScale(scale);
-		}
-	} else if(currentPPType_ == PostProcess::Type::BoxFilter){
-		static int k = 1;
-		if(ImGui::SliderInt("Kernel Size (k)##PostProcess",&k,0,5)){
-			postProcess_->SetKernelSize(k);
-		}
-	} else if(currentPPType_ == PostProcess::Type::GaussianBlur){
-		static int k = 2;
-		if(ImGui::SliderInt("Blur Strength##Gaussian",&k,0,10)){
-			postProcess_->SetKernelSize(k);
-		}
-	}else if(currentPPType_ == PostProcess::Type::Outline){
-		// アウトラインフィルターのパラメータ調整
-	}
-
-	ImGui::End();
+    ImGui::End();
 #endif
 }
